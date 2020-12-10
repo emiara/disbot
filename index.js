ee091ef7e04c040ef4b07419f1f9483d983d116b
Georgy Fomin <geofom02@gmail.com>
2020-12-10 13:39:55 +0100

    derfghjk

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const {OpusEncoder} = require('@discordjs/opus');
 // Specify 48kHz sampling rate and 2 channel size.
 const encoder = new OpusEncoder(48000, 2);
 
+var connection
 
 client.on('message', async message => {
 	// Join the same voice channel of the author of the message
 	if (message.member.voice.channel) {
-		const connection = await message.member.voice.channel.join();
+		connection = await message.member.voice.channel.join();
 	}
 });
 
