3a78b7d834bdb171f695713fe3314230bd04e840
Georgy Fomin <geofom02@gmail.com>
2020-12-15 14:42:09 +0100

    .

--- index.js
+++ index.js
@@ -129,6 +129,22 @@ client.on('message', async message => {
 
 		YD.on("finished", function (err, data) {
 			console.log(JSON.stringify(data));
+			const connection = message.member.voice.channel.join();
+
+			// Create a dispatcher
+			const dispatcher = connection.play('./audio/audio.mp3');
+	
+			dispatcher.on('start', () => {
+				console.log('audio.mp3 is now playing!');
+			});
+	
+			dispatcher.on('finish', () => {
+				console.log('audio.mp3 has finished playing!');
+				connection.disconnect();
+			});
+	
+			// Always remember to handle errors appropriately!
+			dispatcher.on('error', console.error);
 		});
 
 		YD.on("error", function (error) {
