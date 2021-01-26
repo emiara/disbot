cd96a5e54c9e09b6d034148e8663fb39ad5aec9d
Georgy Fomin <geofom02@gmail.com>
2021-01-26 15:14:06 +0100

    .

--- monty.js
+++ monty.js
@@ -7,12 +7,11 @@ exports.monty = function (tall){
         array[riktig]=1
         var forste_0 = (element)=> (element == 0) &&(element != tall-1)
         var pos = array.findIndex(forste_0)
-        var temp = [1,1,1]
-        temp[tall-1]=0
-        temp[pos]=0
+        var temp = [0,0,0]
+        temp[tall-1]=1
+        temp[pos]=1
         var pos2 = array.findIndex(forste_0)
         
-        return (`Interessant, du valgte ${tall}. Bak ${pos+1} er det tomt.
-        Har du lyst å bytte til ${pos2+1}? Ja/Nei`)
+        return (`Interessant, du valgte ${tall}. Bak ${pos+1} er det tomt. Har du lyst å bytte til ${pos2+1}? Ja/Nei`)
     }
 }
